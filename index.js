bb3c780f4150fadb300f1555834de4c5217ccc32
Victor <victorrodrigues288@gmail.com>
2025-12-24 15:32:59 -0300

    Iniciando o backend

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import consign from "consign";
+
+const PORT = 4000;
+
+const app = express();
+
+app.set("json spaces", 4);
+
+consign()
+.include("models")
+.then("routes")
+.into(app);
+
+app.listen(PORT, () => {
+    console.log(`NTask API - porta ${PORT}`)
+});
